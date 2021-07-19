724cc0ae81858e38c6e7baef0983b6119918a16c
kimdu0 <68895169+kimdu0@users.noreply.github.com>
2021-07-20 00:59:21 +0900

    Update add-nbo.cpp

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 	fread(&tho, sizeof(tho), 1, thousand);
 	fread(&five, sizeof(five), 1, five_hundred);
 
-	uint32_t sum = ntohl(tho)+ + ntohl(five);
+	uint32_t sum = ntohl(tho) + ntohl(five);
 
 	printf("\n%d(0x%x) + %d(0x%x) = %d(0x%x)", ntohl(tho), ntohl(tho), ntohl(five), ntohl(five), sum, sum);
 
